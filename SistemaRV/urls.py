f8f0ca8514dea1d336e884abca32140809755721
JoseAguilar <joseaguilarsalazar2004@gmail.com>
2024-09-25 11:29:48 -0500

    refactor: configured documentation

--- SistemaRV/urls.py
+++ SistemaRV/urls.py
@@ -39,4 +39,5 @@ urlpatterns = [
 
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
