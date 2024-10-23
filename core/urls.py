5b2094d77ef7d366d889bc5d8e863a90ac20bdd5
JoseAguilar <joseaguilarsalazar2004@gmail.com>
2024-10-23 16:34:15 -0500

    feat: added codes for notas de credito y debito and all their models, views, etc as well as the tipo operacion codes

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,11 @@ from .views import (
     ComprobanteItemListCreateView,
     ComprobanteItemRetrieveUpdateDestroyView,
     buscar_cliente,
+    Catalogo09TipoNotaDeCreditoListCreateView, Catalogo09TipoNotaDeCreditoRetrieveUpdateDestroyView,
+    Catalogo10TipoNotaDeDebitoListCreateView, Catalogo10TipoNotaDeDebitoRetrieveUpdateDestroyView,
+    Catalogo51TipoDeOperacionListCreateView, Catalogo51TipoDeOperacionRetrieveUpdateDestroyView,
+    NotaCreditoListCreateView, NotaCreditoRetrieveUpdateDestroyView,
+    NotaDebitoListCreateView, NotaDebitoRetrieveUpdateDestroyView
 )
 
 urlpatterns = [
@@ -23,4 +28,19 @@ urlpatterns = [
     path('comprobante-items/<int:pk>/', ComprobanteItemRetrieveUpdateDestroyView.as_view(), name='comprobante-item-retrieve-update-destroy'),
 
     path('buscar-cliente/', buscar_cliente, name='buscar_cliente'),
+    
+    path('catalogo09/', Catalogo09TipoNotaDeCreditoListCreateView.as_view(), name='catalogo09-list-create'),
+    path('catalogo09/<pk>/', Catalogo09TipoNotaDeCreditoRetrieveUpdateDestroyView.as_view(), name='catalogo09-detail'),
+    
+    path('catalogo10/', Catalogo10TipoNotaDeDebitoListCreateView.as_view(), name='catalogo10-list-create'),
+    path('catalogo10/<pk>/', Catalogo10TipoNotaDeDebitoRetrieveUpdateDestroyView.as_view(), name='catalogo10-detail'),
+    
+    path('catalogo51/', Catalogo51TipoDeOperacionListCreateView.as_view(), name='catalogo51-list-create'),
+    path('catalogo51/<pk>/', Catalogo51TipoDeOperacionRetrieveUpdateDestroyView.as_view(), name='catalogo51-detail'),
+    
+    path('nota-credito/', NotaCreditoListCreateView.as_view(), name='nota-credito-list-create'),
+    path('nota-credito/<pk>/', NotaCreditoRetrieveUpdateDestroyView.as_view(), name='nota-credito-detail'),
+    
+    path('nota-debito/', NotaDebitoListCreateView.as_view(), name='nota-debito-list-create'),
+    path('nota-debito/<pk>/', NotaDebitoRetrieveUpdateDestroyView.as_view(), name='nota-debito-detail'),
 ]
