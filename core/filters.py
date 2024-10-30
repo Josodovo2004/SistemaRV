4f4d3ca13423f67b8de39d23bc434aaaa44340ae
JoseAguilar <joseaguilarsalazar2004@gmail.com>
2024-10-30 10:33:49 -0500

    refactor: changed how comprobante filter searches for owner

--- core/filters.py
+++ core/filters.py
@@ -22,9 +22,9 @@ class EntidadFilter(django_filters.FilterSet):
     numeroDocumento = django_filters.CharFilter(lookup_expr='icontains')
     razonSocial = django_filters.CharFilter(lookup_expr='icontains')
     nombreComercial = django_filters.CharFilter(lookup_expr='icontains')
-    ubigeo = django_filters.CharFilter(field_name='ubigeo__nombre', lookup_expr='icontains')
+    ubigeo = django_filters.CharFilter(field_name='ubigeo__codigo', lookup_expr='icontains')
     direccion = django_filters.CharFilter(lookup_expr='icontains')
-    codigoPais = django_filters.CharFilter(field_name='codigoPais__nombre', lookup_expr='icontains')
+    codigoPais = django_filters.CharFilter(field_name='codigoPais__pais', lookup_expr='icontains')
 
     class Meta:
         model = Entidad
