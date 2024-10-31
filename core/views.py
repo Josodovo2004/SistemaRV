b7c57a62c9fbfaf3eefda7832760c828e3050b7f
JoseAguilar <joseaguilarsalazar2004@gmail.com>
2024-10-31 11:24:26 -0500

    fear: added celular field in Entidad

--- core/views.py
+++ core/views.py
@@ -718,7 +718,46 @@ class GeneratePresignedUrlView(APIView):
         }, status=status.HTTP_200_OK)
         
 class ConsultarCliente(APIView):
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = []
     
+    @swagger_auto_schema(
+        operation_description="Consulta un cliente por número de documento.",
+        manual_parameters=[
+            openapi.Parameter(
+                'numero_documento', 
+                openapi.IN_QUERY, 
+                description="Número de documento del cliente", 
+                type=openapi.TYPE_STRING,
+                required=True
+            )
+        ],
+        responses={
+            200: openapi.Response(
+                description="Datos del cliente",
+                schema=EntidadSerializer(),  # Ensure EntidadSerializer is a valid schema
+            ),
+            400: openapi.Response(
+                description="Error: Número de documento es requerido",
+                schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'error': openapi.Schema(type=openapi.TYPE_STRING, description='Mensaje de error'),
+                    }
+                ),
+            ),
+            404: openapi.Response(
+                description="Error: Cliente no encontrado",
+                schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'error': openapi.Schema(type=openapi.TYPE_STRING, description='Mensaje de error'),
+                    }
+                ),
+            ),
+        }
+    )
+    @jwt_required
     def get(self, request):
         documentNumber = request.GET.get('numero_documento')  # Use request.GET for GET requests
 
