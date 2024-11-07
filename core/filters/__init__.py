59800df5f86391eca70a9f75c0323bdb3b5e8b54
JoseAguilar <joseaguilarsalazar2004@gmail.com>
2024-11-07 12:07:44 -0500

    midified filter folder structure

--- core/filters/__init__.py
+++ core/filters/__init__.py
@@ -1,20 +1,20 @@
-from catalogo01Filter import Catalogo01TipoDocumentoFilter
-from catalogo06Filter import Catalogo06DocumentoIdentidadFilter
-from catalogo09Filter import Catalogo09TipoNotaDeCreditoFilter
-from catalogo10Filter import Catalogo10TipoNotaDeDebitoFilter
-from catalogo15Filter import Catalogo15ElementosAdicionalesFilter
-from catalogo51Filter import Catalogo51TipoDeOperacionFilter
-from codigoMonedaFilter import CodigoMonedaFilter
-from codigoPaisFilter import CodigoPaisFilter
-from comprobanteFilter import ComprobanteFilter
-from comprobanteItemFilter import ComprobanteItemFilter
-from entidadFilter import EntidadFilter
-from estadoDocumentoFilter import EstadoDocumentoFilter
-from notaCreditoFilter import NotaCreditoFilter
-from notaDebito import NotaDebitoFilter
-from tipoOperacionFilter import TipoOperacionFilter
-from tipoPagoFilter import TipoPagoFilter
-from ubigeoFilter import UbigeoFilter
+from .catalogo01Filter import Catalogo01TipoDocumentoFilter
+from .catalogo06Filter import Catalogo06DocumentoIdentidadFilter
+from .catalogo09Filter import Catalogo09TipoNotaDeCreditoFilter
+from .catalogo10Filter import Catalogo10TipoNotaDeDebitoFilter
+from .catalogo15Filter import Catalogo15ElementosAdicionalesFilter
+from .catalogo51Filter import Catalogo51TipoDeOperacionFilter
+from .codigoMonedaFilter import CodigoMonedaFilter
+from .codigoPaisFilter import CodigoPaisFilter
+from .comprobanteFilter import ComprobanteFilter
+from .comprobanteItemFilter import ComprobanteItemFilter
+from .entidadFilter import EntidadFilter
+from .estadoDocumentoFilter import EstadoDocumentoFilter
+from .notaCreditoFilter import NotaCreditoFilter
+from .notaDebito import NotaDebitoFilter
+from .tipoOperacionFilter import TipoOperacionFilter
+from .tipoPagoFilter import TipoPagoFilter
+from .ubigeoFilter import UbigeoFilter
 
 
 __all__ = ["Catalogo01TipoDocumentoFilter",
