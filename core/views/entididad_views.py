1fa90ba6bea7fef9dbad02a3ec7a51910e6f9908
Jose <joseaguilarsalazar2004@gmail.com>
2024-11-06 22:24:48 -0500

    refactor: 1

--- core/views/entididad_views.py
+++ core/views/entididad_views.py
@@ -17,28 +17,6 @@ class EntidadListCreateView(generics.ListCreateAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_class = EntidadFilter
     
-    def list(self, request, *args, **kwargs):
-        # Call the original 'list' method to get the default response
-        response = super().list(request, *args, **kwargs)
-        data = response.data
-        # Modify the data in the response
-        if 'respuesta_simple' not in request.data:
-    # Do something if 'respuesta_simple' key is missing:
-            for i in range(len(data['results'])):
-                if isinstance(data['results'][i], dict) and 'id' in data['results'][i]:
-                    # Retrieve the Entidad object
-                    entidad = self.get_queryset().filter(id=data['results'][i]['id']).first()
-                    if entidad:
-                        # Add nested serialized data to the response
-                        data['results'][i]['ubigeo'] = UbigeoSerializer(entidad.ubigeo).data
-                        data['results'][i]['codigoPais'] = CodigoPaisSerializer(entidad.codigoPais).data
-                        data['results'][i]['tipoDocumento'] = Catalogo06DocumentoIdentidadSerializer(entidad.tipoDocumento).data
-                    
-        response.data = data
-
-        # Return the modified response
-        return Response(response.data)
-    
     @jwt_required
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
@@ -48,26 +26,6 @@ class EntidadRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EntidadSerializer
     authentication_classes = [CustomJWTAuthentication]
     permission_classes = []
-
-    def retrieve(self, request, *args, **kwargs):
-        # Call the original 'retrieve' method to get the default response
-        response = super().retrieve(request, *args, **kwargs)
-
-        # Modify the data in the response
-        if isinstance(response.data, dict) and 'id' in response.data:
-            # Retrieve the Entidad object
-            entidad = self.get_object()
-            
-            # Add nested serialized data to the response
-            ubigeo = Ubigeo.objects.filter(codigo= entidad.ubigeo.codigo).first()
-            response.data['ubigeo'] = UbigeoSerializer(ubigeo).data
-            codigoPais = CodigoPais.objects.filter(codigo= entidad.codigoPais.codigo).first()
-            response.data['codigoPais'] = CodigoPaisSerializer(codigoPais).data
-            tipoDocumento = Catalogo06DocumentoIdentidad.objects.filter(codigo= entidad.tipoDocumento.codigo).first()
-            response.data['tipoDocumento'] = Catalogo06DocumentoIdentidadSerializer(tipoDocumento).data
-
-        # Return the modified response
-        return Response(response.data)
     
     @jwt_required
     def put(self, request, *args, **kwargs):
