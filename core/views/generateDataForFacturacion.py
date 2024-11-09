9a9cc1d508d1de814a1ed258770ebab789b6b9c3
JoseAguilar <joseaguilarsalazar2004@gmail.com>
2024-11-09 10:56:36 -0500

    feat: item data in generateDataForFacturacion test1

--- core/views/generateDataForFacturacion.py
+++ core/views/generateDataForFacturacion.py
@@ -6,6 +6,7 @@ from SistemaRV.decorators import CustomJWTAuthentication
 from rest_framework.views import APIView
 from rest_framework import status
 import requests
+from core.models import Entidad, Comprobante, ComprobanteItem
 
 class GenerateFacturacionFromIds(APIView):
     authentication_classes = [CustomJWTAuthentication]
@@ -89,6 +90,43 @@ class GenerateFacturacionFromIds(APIView):
                     "error": "Each entry in 'payTerms' must be a dictionary with a 'metodo' key."
                 }, status=status.HTTP_400_BAD_REQUEST)
                 
+        sendData = {
+            "comprobante" : {},
+            "taxes" : {},
+            "items" : [],
+            "payterms" : data["payterms"],
+            "observaciones" : data["observaciones"],
+            "formaPago" : data["formaPago"],
+            "tipo_pdf" : data['tipo_pdf'],
+        }
+        
+        emisor = Entidad.objects.filter(id=data["emisor"]).first()
+        
+        sendData["emisor"] = {
+            "TipoDocumento": emisor.tipoDocumento.codigo,
+                "DocumentoEmisor": emisor.numeroDocumento,
+                "RazonSocialEmisor": emisor.razonSocial,
+                "ubigeo": emisor.ubigeo.codigo,
+                "calle": emisor.direccion,
+                "distrito": emisor.ubigeo.distrito,
+                "provincia": emisor.ubigeo.provincia,
+                "departamento": emisor.ubigeo.departamento,
+                "email" : '',
+                'telefono' : emisor.celular,
+        }
+        
+        adquiriente = Entidad.objects.filter(id=data["adquiriente"]).first()
+        
+        sendData["adquiriente"] = {
+                "TipoDocumentoAdquiriente": adquiriente.tipoDocumento.codigo,
+                "NumeroDocumentoAdquiriente": adquiriente.numeroDocumento,
+                "razonSocial": adquiriente.razonSocial,
+                "CalleComprador": adquiriente.direccion,
+                "distritoComprador": adquiriente.ubigeo.distrito,
+                "provinciaComprador": adquiriente.ubigeo.provincia,
+                "departamentoComprador": adquiriente.ubigeo.departamento
+            }
+        
         try:
             item_ids = [item['id'] for item in data['items']]
             item_detail_url = "http://54.235.246.131:8002//api/custom-item-view/"
@@ -103,14 +141,53 @@ class GenerateFacturacionFromIds(APIView):
                 return Response({"error": "Failed to retrieve item details from custom-item-view."}, status=response.status_code)
             item_details = []
             # Add fetched item details to response data
+            numeroId=1
             for item in items['results']:
+                for value in data['items']:
+                    if value['id'] == item['id']:
+                        cantidad = value['quantity']
+                        
+                dataToAdd = {
+                    "unidadMedida": item["unidadMedida"]["codigo"],
+                    "CantidadUnidadesItem": cantidad,
+                    "totalValorVenta": item["valorUnitario"] * cantidad,
+                    "precioUnitarioConImpuestos": item["valorUnitario"],
+                    "tipoPrecio": item["tipoPrecio"]["codigo"],
+                    "totalTax": 30.51,
+                    "DescripcionItem": item["descripcion"],
+                    "id": f"PROD{numeroId}",
+                    "precioUnitario":84.75,
+                    'codProducto' : item["codigoProducto"]["codigo"],
+                    'descripcion': item["descripcion"],
+                    'tax' : {},
+                }
+                numeroId +=1
+                totaltax = 0
+                totalPercentage = 0
+                for tax in item['taxes']:
+                    totalPercentage += tax['porcentaje']
+                totalPercentage = totalPercentage/100
+                
+                dataToAdd['precioUnitario'] = item['valorUnitario'] * (1-totalPercentage)
+                
+                for tax in item['taxes']:
+                    dataToAdd['tax'][tax['impuesto']['nombre']] = {
+                            "operacionesGravadas": (item['valorUnitario'] * cantidad)*(1-totalPercentage),
+                            "MontoTotalImpuesto": (item['valorUnitario'] * cantidad)*(1-totalPercentage)*(tax['porcentaje']/100),
+                            "cod1": tax['impuesto']["codigo"],
+                            "cod2": tax['impuesto']["nombre"],
+                            "cod3": tax['impuesto']["un_ece_5153"],
+                            "afectacionIGV": tax['afectacion'],
+                    }
+                    totaltax += (item['valorUnitario'] * cantidad)*(1-totalPercentage)*(tax['porcentaje']/100)
+                dataToAdd['totalTax'] = totaltax
                 item_details.append(item)
+                
+                sendData['items'].append(dataToAdd)
             data['item_details'] = item_details
 
         except requests.RequestException as e:
             return Response({"error": f"Internal API call failed: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-                
-        
         
         
         # If all checks pass, return a success response
