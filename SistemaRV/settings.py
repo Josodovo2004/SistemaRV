37a6b82879b18fcac9b0e1db8191525f23dc6f3b
Jose <joseaguilarsalazar2004@gmail.com>
2024-11-12 21:50:34 -0500

    .

--- SistemaRV/settings.py
+++ SistemaRV/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import environ
-from .awsData import sharedKey, dbEngine, nameDb, passwordDb, userDb, hostDb, portDb 
+from .awsData import sharedKey, passwordDb
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
